4ccac3753988c8c7dc4da76dc8d9e08098fe0c15
pavan-pani <pavanpk1012@gmail.com>
2024-02-08 21:42:08 +0530

    DB setUp

---
+++ src/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password"],
+    },
+    isVerfied: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verfiedToken: String,
+    verfiedTokenExpiry: Date,
+
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema)
+
+export default User
