4df308300babb225c816ad5cd01a107f9a7b0890
pavan-pani <pavanpk1012@gmail.com>
2024-02-15 22:49:06 +0530

    adding middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client"
+import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout")
+      router.push("/login")
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
   return (
-    <h1>This is home</h1>
+    <div>
+      <h1>This is home</h1>
+      <button className=" bg-red-600 hover:bg-red-500 text-white font-bold py-2 mt-4 px-4 rounded"
+        onClick={logout}
+      >Logout</button>
+    </div>
+
   );
 }
